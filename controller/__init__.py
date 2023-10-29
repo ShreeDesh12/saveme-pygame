3fbd6c63a8b9d6d102adb984bf15078e2d35c8cb
Shree <shreeniwas.deshpande12@gmail.com>
2023-10-29 12:56:14 +0530

    Updated game controller with background class changes

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,6 +1,9 @@
 import pygame
+
+from background import setup_background
+from validator.validator import Validator
+
 from .event_process import quit_game
-from .validator import Validator
 
 class GameController():
     def __init__(self, game: pygame) -> None:
@@ -9,14 +12,8 @@ class GameController():
         self.clock = None
         self.validator = Validator(game=game)
 
-    def _add_surface(self, x_loc: int=0, y_loc: int=0, pixel_length: int=0, pixel_width: int=0, color: str='Red') -> None:
-        self.validator.validate_surface(x_loc=x_loc, y_loc=y_loc)
-        surface = self.game.Surface((pixel_length, pixel_width))
-        surface.fill(color)
-        self.screen.blit(surface, (x_loc, y_loc))
-
     def _setup_display(self) -> None:
-        self._add_surface(x_loc=-700, y_loc=10, pixel_length=5, pixel_width=380)
+        setup_background(game=self.game, screen=self.screen)
 
     def setup(self) -> None:
         self.screen = self.game.display.set_mode((800, 400))
